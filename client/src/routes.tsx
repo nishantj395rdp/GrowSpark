fcab653aa60572bfa23f57c3bc21e1c145139a60
nishantj395rdp <133849956+nishantj395rdp@users.noreply.github.com>
2025-09-28 12:26:24 +0530

    Update routes.tsx

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,17 +1,20 @@
-import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import Splash from "./page/Splash";
-import Intro from "./page/Intro";
-import App from "./page/App";
+import Introduction from "./page/Introduction";
 import NotFound from "./page/NotFound";
 
-const AppRoutes: React.FC = () => {
+/**
+ * HashRouter avoids server 404s on Vercel for initial testing.
+ * Route `/app` simply redirects to `/intro` because there's no App.tsx file.
+ */
+const AppRoutes = () => {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<Splash />} />
-        <Route path="/intro" element={<Intro />} />
-        <Route path="/app" element={<App />} />
+        <Route path="/intro" element={<Introduction />} />
+        {/* If you add App.tsx later, change the next line to: <Route path="/app" element={<App/>} /> */}
+        <Route path="/app" element={<Navigate to="/intro" replace />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </HashRouter>

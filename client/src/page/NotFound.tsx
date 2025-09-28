c32e93d6493831cc75bb1dc4370dc79806fa6897
nishantj395rdp <133849956+nishantj395rdp@users.noreply.github.com>
2025-09-28 12:22:29 +0530

    Update NotFound.tsx

--- client/src/page/NotFound.tsx
+++ client/src/page/NotFound.tsx
@@ -1,6 +1,6 @@
-import { FC } from "react";
+import React from "react";
 
-const NotFound: FC = () => {
+const NotFound: React.FC = () => {
   return (
     <div style={{ textAlign: "center", marginTop: "40px" }}>
       <h1>404 - Page Not Found</h1>

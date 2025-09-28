4826dc5c43ceb742d3f45d002ab05e69f89837db
nishantj395rdp <133849956+nishantj395rdp@users.noreply.github.com>
2025-09-28 12:13:26 +0530

    Create NotFound.tsx

---
+++ client/src/page/NotFound.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const NotFound: FC = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "40px" }}>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you’re looking for doesn’t exist.</p>
+    </div>
+  );
+};
+
+export default NotFound;
